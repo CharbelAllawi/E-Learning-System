07509e798b0d3c250bc515c21ea3fcdfe97be282
Charbel Allawi <charbelallawiwork@gmail.com>
2023-08-13 00:15:51 +0300

    Front <> Upload Button>

--- learning-front/src/components/miniCard/index.jsx
+++ learning-front/src/components/miniCard/index.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
 import './style.css';
 import Quiz from '../Quiz';
-
+import uploadpng from "./upload.png"
 const MiniCard = () => {
   const [showQuiz, setShowQuiz] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleQuizButtonClick = () => {
     setShowQuiz(true);
   };
+  const upload = () => {
 
+  }
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    
+  };
   return (
     <div className="card">
       <ul>
+        <li className="User">
+          <span className="quizTitle">E-learning System</span>
+          <span className='quizdesc'>
+            <span className='quizDescription'>I'm the jazzar and you have a nightmare assignment for today! </span>
+          </span>
+          <div className='uploadcontainer'>
+            <div className='upload'>
+              <p className='submittxt'>Submit your answer!</p>
+              <label className='uploadbtn'>
+                <input
+                  type='file'
+                  style={{ display: 'none' }}
+                  onChange={handleFileChange}
+                />
+                <img src={uploadpng} alt="Upload Icon" />
+              </label>
+            </div>
+          </div>
+        </li>
+      </ul>
+      {/* <ul>
         <li className="User">
           <span className="quizTitle">Quiz: React Hooks</span>
           <span className='quizdesc'>
@@ -23,7 +51,7 @@ const MiniCard = () => {
             <Quiz />
           )}
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
