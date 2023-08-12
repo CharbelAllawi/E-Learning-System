0138477af3a2d8c6fb2088c1e5b5341bedeed6cb
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-13 00:10:19 +0300

    Back <> Fixing CourseController & DataBase

---
+++ learning-server/database/migrations/2023_08_12_204925_add_column_to_questions_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->text('correct_answer')->default('');
+        });
+    }
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('correct_answer');
+        });
+    }
+};
