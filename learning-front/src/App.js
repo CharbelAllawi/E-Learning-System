53823035683ee7c6c5c6175c398986d988cbca1a
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-14 18:31:39 +0300

    Front <> Adding Children Enrollments For Parents

--- learning-front/src/App.js
+++ learning-front/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Modal from './components/modal';
 import LandingPage from './components/landing';
 import NavBar from './components/navbar';
-import HomePage from './pages/studentHome/index';
+import HomePage from './pages/studentHome';
 import { EnrollmentStateProvider } from './global/context';
 import StudentCard from './components/studentCard';
 import ParentHome from './pages/parentHome';
@@ -20,7 +20,7 @@ function App() {
           <Routes>
             <Route path='/' element={<LandingPage />}></Route>
             <Route path='/home' element={ 
-              (localStorage.getItem('usertype') === 'Student') ? <HomePage />: <ParentHome/> } />
+              (localStorage.getItem('usertype') === 'Student') ? (<HomePage />): (<ParentHome/>) } />
           </Routes>
         </EnrollmentStateProvider>
       </BrowserRouter>
--- learning-front/src/pages/parentHome/index.jsx
+++ learning-front/src/pages/parentHome/index.jsx
@@ -2,9 +2,27 @@ import {useEffect, useState} from 'react'
 import StudentCard from '../../components/studentCard';
 import { sendRequest } from '../../core/config/request';
 import './styles.css'
+import MyCard from '../../components/courseCard';
 const ParentHome = () => {
 
     const [children, setChildren] = useState([])
+    const [childCourses, setChildCourses] = useState([])
+
+    const getChildCourses = async (child) => {
+        console.log(0)
+        try {
+            const response = await sendRequest({
+                method: "GET",
+                route: `/api/get_courses/${child}`,
+            });
+            console.log(1)
+            setChildCourses(response.courses)
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(2)
+    };
+    
 
     useEffect(() => {
         const getChildrenHandler = async () => {
@@ -25,11 +43,18 @@ const ParentHome = () => {
         <div className='parents-home-body'>
             <div className='student-cards-container'>
                 {children.map(child => (
-                    <StudentCard key={child} childName={child}/>)) 
+                    <StudentCard key={child} childName={child} onCall={() => getChildCourses(child)}/>)) 
                 }
             </div>
-            <div>
-                
+            <div className='Child-courses-contained'>
+                {
+                    (
+                        childCourses.length > 0 ?
+                        childCourses.map(classInfo => (
+                            <MyCard key={classInfo.id} classInfo={classInfo} isEnrolled={classInfo.isEnrolled}/>))
+                            : <span className='empty-child-course-container'>No Courses Found</span>
+                    )
+                }
             </div>
         </div>
         </>
