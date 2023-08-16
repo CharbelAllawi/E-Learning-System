87bd2085061e5004ec5da17b323a9394152812a7
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-16 07:28:19 +0300

    Front <> Adding Logout

--- learning-front/src/components/navbar/index.jsx
+++ learning-front/src/components/navbar/index.jsx
@@ -3,18 +3,43 @@ import { useState, useEffect } from "react"
 import "./styles.css"
 import { useEnrollmentState } from '../../global/context'
 import { useLocation } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+import { sendRequest } from '../../core/config/request'
+
 const NavBar = () => {
 
+    const navigate = useNavigate()
     const { isEnrolled, setIsEnrolled } = useEnrollmentState();
     const [showButtons, setShowButtons] = useState(true);
+    const [showLogout, setShowLogout] = useState(true);
     const location = useLocation();
     useEffect(() => {
+        if (location.pathname == "/"){
+            setShowButtons(false);
+            setShowLogout(false)
+        }
         if (location.pathname == "/" || (localStorage.getItem('usertype') != 4)) {
             setShowButtons(false);
         } else {
             setShowButtons(true);
+            setShowLogout(true)
         }
-      }, [location.pathname]);
+    }, [location.pathname]);
+
+    const logoutHandler = async () => {
+        try {
+            const response = await sendRequest({
+                method: "POST",
+                route: "/api/logout",
+            });
+            if(response.message === "Successfully logged out"){
+                localStorage.clear()
+                navigate('/')
+                window.location.reload(false)
+                } 
+        } catch (error) {
+            console.log(error);
+        }}
 
     return ( 
         <div className='navbar'>
@@ -26,6 +51,7 @@ const NavBar = () => {
                 <button className={isEnrolled ? "navbar-btn" : "navbar-btn active"} onClick={()=> setIsEnrolled(false)}>Browse</button>
                 </>
             )}
+            {showLogout &&<button className={"navbar-btn"} onClick={logoutHandler}>Logout</button>}
             </div>
         </div>
     );
